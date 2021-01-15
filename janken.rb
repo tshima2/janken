f53c3bf8d962bdcf1d727ce368975e80bf8ab194
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-01-15 15:30:18 +0900

    1st edition

---
+++ janken.rb
@@ -0,0 +1,73 @@
+#候補手をconst定義
+$HANDS=["グー", "チョキ", "パー"]
+
+# プレイヤー(自分)に「0 ~ 2」を入力させるロジックを書きます。
+class Player
+  def hand
+    # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
+    puts "数字を入力してください。"
+    #puts "0:グー, 1:チョキ, 2:パー"
+    $HANDS.each_with_index do |val,key|
+      puts "#{key.to_s}:#{val}"
+    end
+
+
+    # 変数「input_hand」にプレイヤーが入力したものを取得して代入します。
+    # ヒント! 「getsメソッド」を使用します。
+    input_hand = gets.to_i
+
+    # 「input_hand」(取得した値)が「0, 1, 2」のいずれかだとwhileから脱出させ、それ以外だと初めから繰り返させます。
+    while true
+      if Range.new(0, $HANDS.length).include?(input_hand)
+        return input_hand
+      else
+        # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
+        puts "もう一度数字を入力してください。"
+        #puts "0:グー, 1:チョキ, 2:パー"
+        $HANDS.each_with_index do |val,key|
+          puts "#{key.to_s}:#{val}"
+        end
+        input_hand=gets.to_i
+      end
+    end
+  end
+end
+
+class Enemy
+  def hand
+    # グー、チョキ、パーの値をランダムに出力させます。
+    return rand(Range.new(0, $HANDS.length))
+  end
+end
+
+class Janken
+
+  def pon(player_hand, enemy_hand)
+    #puts "相手の手は#{ $HANDS[enemy_hand] }です。"
+    describe_enemy_hand="相手の手は#{ $HANDS[enemy_hand] }です。"
+
+    if player_hand == enemy_hand
+      puts "あいこ"
+      #「true」を返してじゃんけんを繰り返し実行させます。
+      return true
+
+    elsif ((player_hand == 0 && enemy_hand == 1) || (player_hand == 1 && enemy_hand == 2) || (player_hand == 2 && enemy_hand == 0))
+      puts "#{describe_enemy_hand}あなたの勝ちです"
+      # whileを終了させる「false」を返してじゃんけんを終了させます。
+      return false
+    else
+      puts "#{describe_enemy_hand}あなたの負けです"
+      # whileを終了させる「false」を返してじゃんけんを終了させます。
+      return false
+    end
+  end
+end
+
+player = Player.new
+enemy = Enemy.new
+janken = Janken.new
+
+next_game=true
+while next_game do
+  return if !(next_game = janken.pon(player.hand, enemy.hand))
+end
